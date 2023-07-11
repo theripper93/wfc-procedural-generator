eedb743d2d160ae297473cbb6a44d567273354bb
theripper93 <tsplab@gmail.com>
2023-07-11 16:04:48 +0200

    Initial commit

---
+++ scripts/main.js
@@ -0,0 +1,9 @@
+import {initConfig} from "./config.js";
+import { registerSettings } from "./settings.js";
+
+export const MODULE_ID = "module-id";
+
+Hooks.on("init", () => {
+    initConfig();
+    registerSettings();
+});
